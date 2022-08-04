7119c6d05c81cffb28439735db360e3ff82a2432
iali <engr.imran_1242@hotmail.com>
2022-08-04 14:58:28 +0200

    mongoose-encryption, First commit

---
+++ app.js
@@ -0,0 +1,71 @@
+require('dotenv').config()
+const express = require('express')
+const ejs = require('ejs')
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+const app = express()
+const _ = require('lodash')
+var encrypt = require('mongoose-encryption')
+
+app.use(bodyParser.urlencoded({ extended: true }))
+app.set('view engine', 'ejs')
+app.use(express.static(__dirname + '/public'))
+mongoose.connect('mongodb://localhost:27017/userDB')
+const userSchema = new mongoose.Schema({
+  username: String,
+  password: String,
+})
+
+const secret = process.env.SECRET
+userSchema.plugin(encrypt, { secret: secret, encryptedFields: ['password'] })
+
+const User = mongoose.model('User', userSchema)
+
+app.get('/', (req, res) => {
+  res.render('home')
+})
+
+app.get('/register', (req, res) => {
+  res.render('register')
+})
+
+app.get('/login', (req, res) => {
+  res.render('login')
+})
+
+app.post('/register', (req, res) => {
+  const email = req.body.username
+  const password = req.body.password
+  const newUser = new User({
+    username: email,
+    password: password,
+  })
+  newUser.save((err) => {
+    if (!err) {
+      res.render('secrets')
+    } else {
+      console.log(err)
+    }
+  })
+})
+app.post('/login', (req, res) => {
+  const usernameUser = req.body.username
+  const passwordUser = req.body.password
+  User.findOne({ username: usernameUser }, (err, userFound) => {
+    if (!err) {
+      if (userFound) {
+        if (userFound.password === passwordUser) {
+          res.render('secrets')
+        } else {
+          res.send('You entered wrong Email or Password!')
+        }
+      }
+    } else {
+      console.log(err)
+    }
+  })
+})
+
+app.listen(3000, () => {
+  console.log('server is running on port 3000.')
+})
