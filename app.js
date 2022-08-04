30502b91442fb14591c482abf65e739f7b4387bc
iali <engr.imran_1242@hotmail.com>
2022-08-04 16:05:35 +0200

    used hasing with sath and bcrypt

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
 const app = express()
 const _ = require('lodash')
-const md5 = require('md5')
+const bcrypt = require('bcrypt')
+const saltRounds = 10
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.set('view engine', 'ejs')
@@ -31,31 +32,38 @@ app.get('/login', (req, res) => {
 })
 
 app.post('/register', (req, res) => {
-  const email = req.body.username
-  const password = md5(req.body.password)
-  const newUser = new User({
-    username: email,
-    password: password,
-  })
-  newUser.save((err) => {
-    if (!err) {
-      res.render('secrets')
-    } else {
-      console.log(err)
-    }
+  bcrypt.hash(req.body.password, saltRounds, function (err, hash) {
+    const email = req.body.username
+    const password = hash
+    const newUser = new User({
+      username: email,
+      password: password,
+    })
+    newUser.save((err) => {
+      if (!err) {
+        res.render('secrets')
+      } else {
+        console.log(err)
+      }
+    })
   })
 })
 app.post('/login', (req, res) => {
   const usernameUser = req.body.username
-  const passwordUser = md5(req.body.password)
+  const passwordUser = req.body.password
   User.findOne({ username: usernameUser }, (err, userFound) => {
     if (!err) {
       if (userFound) {
-        if (userFound.password === passwordUser) {
-          res.render('secrets')
-        } else {
-          res.send('You entered wrong Email or Password!')
-        }
+        bcrypt.compare(passwordUser, userFound.password, function (
+          err,
+          result,
+        ) {
+          if (result === true) {
+            res.render('secrets')
+          } else {
+            res.send('You entered wrong Email or Password!')
+          }
+        })
       }
     } else {
       console.log(err)
